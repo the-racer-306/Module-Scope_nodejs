9243d44733e24eb3860034cccdb53558b66c4b63
the-racer-306 <khaledmamdouh306@gmail.com>
2023-01-29 18:40:59 +0200

    How module scope works

---
+++ iife.js
@@ -0,0 +1,14 @@
+(function () {
+	const superHero = "Batman";
+	console.log(superHero);
+})();
+
+(function () {
+	const superHero = "Superman";
+	console.log(superHero);
+})();
+
+// iife => Immediately Invoked Function Expression
+// it's a javaScript function that runs as soon as it's defined and has it's own scope
+
+// There is proper enCapsulation and reusability is unaffected
---
+++ index.js
@@ -0,0 +1,4 @@
+require("./batman");
+require("./superman");
+
+// if we run the code above both files will run despite that both have the same variable, Due to module scope
